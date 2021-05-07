530a22dddf58527e89e1d25b4f770e78f2c87355
Talha <bsem-f18-206@superior.edu.pk>
2021-05-07 23:30:38 +0500

    Initial commit

---
+++ scrape.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+res=requests.get('https://news.ycombinator.com/front')
+soup=BeautifulSoup(res.text,'html.parser')
+links=soup.select('.storylink')
+subtext=soup.select('.subtext')
+def create_custom_hn(links,subtext):
+	hn=[]
+	for idx,item in enumerate(links):
+		title=links[idx].getText()
+		href=links[idx].get('href',None)
+		vote=subtext[idx].select('.score')
+		if len(vote):
+			points=int(vote[0].getText().replace('points',''))
+			print(points)
+			hn.append({'title':title,'link':href})
+
+	return hn
+(create_custom_hn(links,subtext))
